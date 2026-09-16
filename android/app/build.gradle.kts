41a7c640aa58fbfd6640fbfa4d5ef62e5f0e3524
charan-143 <devanapallycharantej@gmail.com>
2026-09-16 20:58:54 +0530

    build(release): configure release signing keystore and production build target

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,9 +15,19 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("dentara-release-key.jks")
+            storePassword = "dentara2026"
+            keyAlias = "dentara"
+            keyPassword = "dentara2026"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
